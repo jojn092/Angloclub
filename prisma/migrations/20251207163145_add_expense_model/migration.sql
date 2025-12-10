aba72f6d6088247bb08f47b8d9279e329b5ba810
jojn092 <jojn092@gmail.com>
2025-12-10 23:53:57 +0500

    chore: force reset database and remove legacy migrations

--- prisma/migrations/20251207163145_add_expense_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Expense" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "amount" INTEGER NOT NULL,
-    "category" TEXT NOT NULL,
-    "description" TEXT,
-    "date" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "recordedBy" INTEGER
-);
